d3dd892ad2e0ac9f01101d0df0a6150db5209eb3
razin <razinmohammedpt@gmail.com>
2025-01-14 17:04:45 +0530

    Update Google authentication callback URL to use relative path

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(profile);
